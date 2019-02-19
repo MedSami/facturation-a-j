4a24c98240f4bdd85657911f7f6c5e3e3a2368bc
Mohamed Sami Mejri <medsamimejri@gmail.com>
2019-02-19 15:41:20 +0100

    add logo/ login interface / widget ref

--- app/src/main/java/com/amina/facturation/MainActivity.java
+++ app/src/main/java/com/amina/facturation/MainActivity.java
@@ -2,12 +2,20 @@ package com.amina.facturation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+EditText edtPseudo,edtPassword;
+Button btnEntrer,btnInscrire;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtPassword=findViewById(R.id.edtPassword);
+        edtPseudo=findViewById(R.id.edtPseudo);
+        btnEntrer=findViewById(R.id.btnLogin);
+        btnInscrire=findViewById(R.id.btnInscrit);
     }
 }
