0471e7b3b5c0f69f9c947fb24a8520c30b0cbcb7
Mohamed Sami Mejri <medsamimejri@gmail.com>
2019-02-20 12:16:12 +0100

    Login/Interface Facturation Comptant & Credit

--- app/src/main/java/com/amina/facturation/MainActivity.java
+++ app/src/main/java/com/amina/facturation/MainActivity.java
@@ -6,6 +6,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.amina.facturation.API.ApiRequest;
+import com.amina.facturation.API.RetrofitServer;
+import com.amina.facturation.Model.ResponseDataModel;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 EditText edtPseudo,edtPassword;
@@ -32,7 +41,51 @@ Button btnEntrer,btnInscrire;
         btnEntrer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(edtPseudo.getText().toString().equals("")){
+                    Toast.makeText(MainActivity.this, "Remplir Champ Pseudo Svp", Toast.LENGTH_SHORT).show();
+                }else if(edtPassword.getText().toString().equals("")){
+                    Toast.makeText(MainActivity.this, "Remplir Champ Mot De Passe SVP", Toast.LENGTH_SHORT).show();
+                }else {
+                    ApiRequest api= RetrofitServer.getClient().create(ApiRequest.class);
+                    //Instance Call Methode
+                    Call<ResponseDataModel> Login=api.Login(edtPseudo.getText().toString());
+                Login.enqueue(new Callback<ResponseDataModel>() {
+                    @Override
+                    public void onResponse(Call<ResponseDataModel> call, Response<ResponseDataModel> response) {
 
+                        if(response.isSuccessful()){
+                            if(response.body().getResult().get(0).getPseudo().equals(edtPseudo.getText().toString())){
+                                if(response.body().getResult().get(0).getPassword().equals(edtPassword.getText().toString())){
+
+
+                                        Intent intent=new Intent(MainActivity.this,Facturation.class);
+                                        intent.putExtra("idUtilisateur",""+response.body().getResult().get(0).getId());
+                                        startActivity(intent);
+
+
+
+
+                                }else {
+                                    Toast.makeText(MainActivity.this, "Mot De Passe Incorrect", Toast.LENGTH_SHORT).show();
+                                }
+                            }else {
+                                Toast.makeText(MainActivity.this, "Pseudo Incorrect", Toast.LENGTH_SHORT).show();
+
+                            }
+                        }else {
+                            Toast.makeText(MainActivity.this, "Pseudo Incorrect", Toast.LENGTH_SHORT).show();
+                        }
+
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseDataModel> call, Throwable t) {
+                        Toast.makeText(MainActivity.this, "Problem Connexion", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                }
             }
         });
     }
